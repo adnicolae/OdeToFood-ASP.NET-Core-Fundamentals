c70fd4b3c12b14b1303fc074c8a234113e417070
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-10-03 20:44:29 +0100

    Attribute-based routing

--- OdeToFood-CoreFundamentals/Controllers/AboutController.cs
+++ OdeToFood-CoreFundamentals/Controllers/AboutController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood_CoreFundamentals.Controllers
-{
+{ 
+    [Route("[controller]/[action]")]
     public class AboutController
     {
         public string Phone()
